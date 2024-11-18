8573c79ec242a28ff5db40518ec0c1e59b1323ab
hritik602 <hritikrajkarn@gmail.com>
2024-11-18 15:07:29 +0545

    fix: change prefix icon to prefix widget

--- lib/intl_phone_field.dart
+++ lib/intl_phone_field.dart
@@ -400,7 +400,7 @@ class _IntlPhoneFieldState extends State<IntlPhoneField> {
       onFieldSubmitted: widget.onSubmitted,
       magnifierConfiguration: widget.magnifierConfiguration,
       decoration: widget.decoration.copyWith(
-        prefixIcon: _buildFlagsButton(),
+        prefix: _buildFlagsButton(),
         counterText: !widget.enabled ? '' : null,
       ),
       style: widget.style,
